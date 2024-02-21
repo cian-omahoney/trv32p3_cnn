93d71f54ad9c9996440add0cc7497ecd4425c95f
cian.omahoney <cian.omahoney@ucdconnect.ie>
2024-02-21 18:04:56 +0000

    Reduce data memory size from 16 to 14 bit.

--- lib/trv32p3_cnn_define.h
+++ lib/trv32p3_cnn_define.h
@@ -17,7 +17,7 @@
 //  * ePM: 32 bits per word
 //  * eDM: 32 bits per word
 #define MEM_ePM_SZ_NBIT 16
-#define MEM_eDM_SZ_NBIT 16
+#define MEM_eDM_SZ_NBIT 14
 
 // Note: this header file is included almost everywhere, e.g. in the processing
 // of nML, PDG, C/C++, BCF, ... files. Be careful which preprocessor macros
